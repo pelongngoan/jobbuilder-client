066f417e04ecf5480c9524199a63984d9bfc00e5
thanhflong <long.tl446@gmail.com>
2025-04-12 23:30:29 +0700

    [Feature] Remove and Update project structure

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
+import { StrictMode } from "react";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routes/Routes.tsx";
+import { ThemeProvider } from "./context/ThemeContext";
 
-// createRoot(document.getElementById("root")!).render(
-//   <StrictMode>
-//     <RouterProvider router={router} />
-//   </StrictMode>
-// );
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <ThemeProvider>
+      <RouterProvider router={router} />
+    </ThemeProvider>
+  </StrictMode>
+);
--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,34 +1,56 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
-import Login from "../pages/Login";
-import Home from "../pages/Home";
-import { AuthProvider } from "../context/useAuth";
-import { QueryClientProvider } from "@tanstack/react-query";
-import { queryClient } from "../lib/queryClient";
+import { Register } from "../pages/Register";
+import { Login } from "../pages/Login";
+import { Home } from "../pages/Home";
+import { Resumes } from "../pages/Resumes";
+import { Search } from "../pages/Search";
+import { Profile } from "../pages/Profile";
+import { PostJob } from "../pages/PostJob";
+import { CreateResume } from "../pages/CreateResume";
+import { ResumeEditor } from "../pages/ResumeEditor";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: (
-      <QueryClientProvider client={queryClient}>
-        <AuthProvider>
-          <App />
-        </AuthProvider>
-      </QueryClientProvider>
-    ),
+    element: <App />,
     children: [
       {
-        path: "/",
+        path: "/home",
         element: <Home />,
+      },
+      {
+        path: "/resumes",
+        element: <Resumes />,
       },
       {
         path: "/login",
         element: <Login />,
       },
-      // {
-      //   path: "/register",
-      //   element: <Register />,
-      // },
+      {
+        path: "/register",
+        element: <Register />,
+      },
+      {
+        path: "/search",
+        element: <Search />,
+      },
+      {
+        path: "/profile",
+        element: <Profile />,
+      },
+      {
+        path: "/post-job",
+        element: <PostJob />,
+      },
+      {
+        path: "/create-resume",
+        element: <CreateResume />,
+      },
+      {
+        path: "/resume-editor/:templateId",
+        element: <ResumeEditor />,
+      },
     ],
   },
 ]);
